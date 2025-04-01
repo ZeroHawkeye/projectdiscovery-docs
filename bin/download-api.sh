51ae98d573fb5e67c19f8cda7cfa2729db604836
zero <166997982@qq.com>
2025-04-01 15:17:40 +0800

    zh-CN

---
+++ bin/download-api.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+echo "Downloading file"
+wget -O openapi.yaml https://stoplight.io/api/v1/projects/projectdiscovery/pdcp-api-new/nodes/openapi.yaml\?fromExportButton\=true\&snapshotType\=http_service
